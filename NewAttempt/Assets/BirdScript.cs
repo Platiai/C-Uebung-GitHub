7aec039dcf9973c737853e0911400f30f5eb6526
Platiai <alenaendlicher@gmail.com>
2023-12-15 17:17:16 +0100

    Bird in and script new

--- NewAttempt/Assets/BirdScript.cs
+++ NewAttempt/Assets/BirdScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movingobstacle : MonoBehaviour
+public class BirdScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
