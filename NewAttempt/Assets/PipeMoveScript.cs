b36a283d24f407cdc11dc1ca67bd6cfb83a8cab3
Platiai <alenaendlicher@gmail.com>
2023-12-15 17:38:44 +0100

    Birdmove

--- NewAttempt/Assets/PipeMoveScript.cs
+++ NewAttempt/Assets/PipeMoveScript.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BirdScript : MonoBehaviour
+public class PipeMoveScript : MonoBehaviour
 {
-    public Rigidbody2D myrigidbody;
-    public float flapstrenght;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +13,6 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) == true)
-        {
-            myrigidbody.velocity = Vector2.up * flapstrenght;
-        }
         
     }
 }
